c055c79cd3e471a4a240fb4c41c73c71bde876e1
nicolasgsantana <nicolas07san@gmail.com>
2026-03-24 18:23:04 -0300

    feat: finish implementation

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nde-sant <nde-sant@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/24 17:30:54 by nde-sant          #+#    #+#             */
+/*   Updated: 2026/03/24 18:12:22 by nde-sant         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	std::string str;
 
+	if (argc < 2)
+		str = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else 
+	{
+		for (int i = 1; i < argc; i++)
+			str.append(argv[i]);
+		std::transform (str.begin(), str.end(), str.begin(), ::toupper);
+	}
+	std::cout << str << std::endl;
 	return (0);
 }
