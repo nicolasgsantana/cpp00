0984bb47c1d967538945b4bfce5513e1b24803fa
nicolasgsantana <nicolas07san@gmail.com>
2026-03-31 12:01:02 -0300

    feat: add Contact class implementation

---
+++ ex01/inc/Contact.hpp
@@ -0,0 +1,19 @@
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+# include <string>
+
+class Contact
+{
+public:
+	std::string	firstName;
+	std::string	lastName;
+	std::string	nickname;
+	std::string	title;
+	std::string	company;
+	std::string	officeAddress;
+	std::string	officePhone;
+	std::string	homePhone;
+};
+
+#endif
