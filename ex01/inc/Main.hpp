54ffca49c35ce4617081439aea17eeb9de2d0f7f
nicolasgsantana <nicolas07san@gmail.com>
2026-04-16 12:24:39 -0300

    feat: add basic repl

--- ex01/inc/Main.hpp
+++ ex01/inc/Main.hpp
@@ -2,5 +2,13 @@
 # define MAIN_HPP
 
 #include "PhoneBook.hpp"
+#include <iostream>
+#include <iomanip>
+
+#define HEADER     "============= PHONEBOOK 85 ============="
+#define ADD_MSG    "ADD    - save a new contact"
+#define SEARCH_MSG "SEARCH - display a specific contact"
+#define EXIT_MSG   "EXIT   - exit the program"
+#define INPUT_MSG  "Enter your option: "
 
 #endif
--- ex01/src/Main.cpp
+++ ex01/src/Main.cpp
@@ -2,6 +2,23 @@
 
 int	main(void)
 {
-	PhoneBook phonebook;
+	PhoneBook	phoneook;
+	std::string	option;
+
+	do {
+		std::cout << HEADER << std::endl;
+		std::cout << ADD_MSG << std::endl;
+		std::cout << SEARCH_MSG << std::endl;
+		std::cout << EXIT_MSG << std::endl;
+		std::cout << INPUT_MSG;
+		getline(std::cin, option);
+		std::cout << std::endl;
+		if (option == "ADD")
+			std::cout << "IMPLEMENT ADD" << std::endl;
+		else if (option == "SEARCH")
+			std::cout << "IMPLEMENT SEARCH" << std::endl;
+		else
+			std::cout << "INVALID OPTION" << std::endl;
+	} while (option != "EXIT");
 	return (0);
 }
