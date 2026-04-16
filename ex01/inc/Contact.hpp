a7f8058cf6201093dfa72fc881bf4bd483028cad
nicolasgsantana <nicolas07san@gmail.com>
2026-04-16 12:23:52 -0300

    fix: remove unncessary properties

--- ex01/inc/Contact.hpp
+++ ex01/inc/Contact.hpp
@@ -9,11 +9,8 @@ public:
 	std::string	firstName;
 	std::string	lastName;
 	std::string	nickname;
-	std::string	title;
-	std::string	company;
-	std::string	officeAddress;
-	std::string	officePhone;
-	std::string	homePhone;
+	std::string	phoneNumber;
+	std::string	darkestSecret;
 };
 
 #endif
